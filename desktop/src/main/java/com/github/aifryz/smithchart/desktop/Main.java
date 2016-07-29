95d1867ccea6da69b105bd40bed177295ab47b68
Robert Bicz <robal10@poczta.fm>
2016-07-29 22:47:28 +0200

    Added FXML GUI loading

--- desktop/src/main/java/com/github/aifryz/smithchart/desktop/Main.java
+++ desktop/src/main/java/com/github/aifryz/smithchart/desktop/Main.java
@@ -5,6 +5,8 @@ package com.github.aifryz.smithchart.desktop; /**
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -21,20 +23,17 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage){
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
+            Scene scene = new Scene(root, 300, 275);
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+            primaryStage.setTitle("FXML Welcome");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }catch (Exception e)
+        {
+            System.out.println("Exception loading fxml: "+e.toString()+" : "+e.getCause()+ e.getMessage()+e.getStackTrace());
+        }
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
     }
 }
